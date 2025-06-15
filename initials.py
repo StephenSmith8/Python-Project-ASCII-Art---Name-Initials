777c062bf084489d14da1e16ba0d0f335929bc95
Stephen Smith <112740787+StephenSmith8@users.noreply.github.com>
2025-06-15 14:14:25 -0400

    Create initials.py

---
+++ initials.py
@@ -0,0 +1,42 @@
+#My full name
+full_name = "Stephen Michael Smith"
+
+#A fun fact about myself
+fun_fact = "I am a billionaire"
+
+# Defines first initial variable
+first_initial = """
+ SSS 
+S   S
+S
+ SSS 
+    S
+S   S
+ SSS
+
+""" 
+
+#Defines second initial variable
+second_initial = """
+M   M
+MM MM
+MM MM
+M M M
+M   M
+M   M
+M   M   
+
+"""
+
+#Defines third initial variable
+third_initial = """
+ SSS 
+S   S
+S
+ SSS 
+    S
+S   S
+ SSS
+
+""" 
+print (first_initial, second_initial, third_initial)
